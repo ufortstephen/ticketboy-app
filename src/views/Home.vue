5d2aab80b76439e22966d53f5ac67cf3f2daafb6
ufortstephen <ufortstephen@gmail.com>
2021-08-27 14:39:19 +0100

    mail service added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,19 +23,51 @@
           </p>
           <div class="home__buttons__container">
             <div>
-              <el-input
-                placeholder="Enter your email address"
-                v-model="input"
-                clearable
-                class="home__input"
+              <!-- Element UI Form and Input start -->
+              <el-form
+                :model="dynamicValidateForm"
+                ref="dynamicValidateForm"
+                label-width="120px"
+                class="demo-dynamic"
               >
-              </el-input>
+                <el-form-item
+                  prop="email"
+                  placeholder="Enter your email address"
+                  label=""
+                  :rules="[
+                    {
+                      required: true,
+                      message: 'Please input email address',
+                      trigger: 'blur',
+                    },
+                    {
+                      type: 'email',
+                      message: 'Please input correct email address',
+                      trigger: ['blur', 'change'],
+                    },
+                  ]"
+                >
+                  <el-input
+                    v-model="dynamicValidateForm.email"
+                    placeholder="Enter your email address"
+                    class="home__input"
+                  ></el-input>
+                </el-form-item>
+              </el-form>
+              <!-- Element UI Form and Input end -->
             </div>
+
+            <!-- Submit Button -->
             <div>
-              <button class="btn w-100 home__button__blue">
+              <button
+                class="btn w-100 home__button__blue"
+                @click="submitForm('dynamicValidateForm')"
+                id="getAccessBtn"
+              >
                 Get Early Access
               </button>
             </div>
+            <!-- Submit Button -->
           </div>
         </div>
         <!-- Desktop Images -->
@@ -63,15 +95,82 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
 
+
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
+ <script type="text/javascript">
+(function () {
+  emailjs.init("user_clplcO5e1dK5gp2xPVmAz");
+})();
+</script>
+
+
+
+<script>
 export default {
-  name: "Home",
   data() {
     return {
-      input: "",
+      dynamicValidateForm: {
+        email: "",
+      },
     };
+  },
+  methods: {
+    submitForm(formName) {
+
+      // Validate Form
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+
+
+          // Try function to send mail
+          try {
+            
+            // Getting button to manipulate textContent
+            const btnGetAccess = document.getElementById("getAccessBtn");
+            btnGetAccess.textContent = "Requesting....";
+
+            // TempParams Defined to specify user email
+            let tempParams = {
+              user_email: this.dynamicValidateForm.email,
+            };
+
+            // ServiceId and templateId defined and assigned
+            const serviceID = "service_66b4c6r";
+            const templateID = "template_koxidqv";
+
+            // Send mail function
+            emailjs.send(serviceID, templateID, tempParams).then(
+              () => {
+                btnGetAccess.textContent = "Get Early Access";
+                this.dynamicValidateForm.email = "";
+
+                // Trigger success message
+                this.$message({
+                  message:
+                    "Congrats, You have been added to TicketBoy's waiting list.",
+                  type: "success",
+                });
+              },
+
+              // catch error
+              (err) => {
+                // set default
+                btnGetAccess.textContent = "Get Early Access";
+                this.dynamicValidateForm.email = "";
+                alert(JSON.stringify(err));
+              }
+            );
+          } catch (error) {
+            console.log(error);
+          }
+        } else {
+          // Catch submit button error
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
   },
 };
 </script>
